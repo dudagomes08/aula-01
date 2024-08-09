939bf2c22c89c5b2f99d86f60e3f2220dba61eb0
dudagomes08 <00001088165515sp@al.educacao.sp.gov.br>
2024-08-09 16:14:46 -0300

    adicionado

--- sketch.js
+++ sketch.js
@@ -1,1 +1,149 @@
-
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  
+    let palavra = "Caminhante";
+  }
+  function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    fill("black");
+    let palavra = "Caminhante";
+    text(palavra, 200, 200);
+  }
+  function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    fill("black");
+    let palavra = "Caminhante";
+    textAlign(CENTER, CENTER);
+    text(palavra, 200, 200);
+  }
+  function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    fill("black");
+    let palavra = "Caminhante";
+    textSize(64);
+    textAlign(CENTER, CENTER);
+    text(palavra, 200, 200);
+  }
+  function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    fill("black");
+    textSize(64);
+    textAlign(CENTER, CENTER);
+    
+    let palavra = "Caminhante";
+    text(palavra, 200, 200);
+  }
+  function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background("white");
+    fill("black");
+    textSize(64);
+    textAlign(CENTER, CENTER);
+    
+    let palavra = "Caminhante";
+    text(palavra, 200, 200);
+  }
+  function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background("white");
+    fill("black");
+    textSize(64);
+    textAlign(CENTER, CENTER);
+    
+    if(mouseX < 50) {
+      let palavra = "C";
+      text(palavra, 200, 200);
+    } else {
+      let palavra = "Caminhante";
+      text(palavra, 200, 200);
+    }
+  }
+  function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background("white");
+    fill("black");
+    textSize(64);
+    textAlign(CENTER, CENTER);
+    
+    if(mouseX < 50) {
+      let palavra = "C";
+      text(palavra, 200, 200);
+    } else if(mouseX < 100) {
+      let palavra = "Ca";
+      text(palavra, 200, 200);
+    } else {
+      let palavra = "Caminhante";
+      text(palavra, 200, 200);
+    }
+  
+  }
+  function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background("white");
+    fill("black");
+    textSize(64);
+    textAlign(CENTER, CENTER);
+    
+    let maximo = width;
+    let minimo = 0;
+    // mouseX, 0, width ==> 0, 10
+    if(mouseX < 50) {
+      let palavra = "C";
+      text(palavra, 200, 200);
+    } else if(mouseX < 100) {
+      let palavra = "Ca";
+      text(palavra, 200, 200);
+    } else {
+      let palavra = "Caminhante";
+      text(palavra, 200, 200);
+    }
+  
+  }
+  // código omitido
+    
+    let maximo = width;
+    let minimo = 0;
+    // mouseX, 0, width ==> 0, 10
+    console.log("Caminhante".length);
+    
+  // código omitido
+  // código omitido
+    
+    let maximo = width;
+    let minimo = 0;
+    // mouseX, 0, width ==> 0, palavra.length
+    
+  // código omitido
+  
